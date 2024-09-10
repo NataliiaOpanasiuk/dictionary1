2033f4c1d166f01b102ff87044439fa11bad6186
NataliiaOpanasiuk <nataopanasiuk@gmail.com>
2024-09-09 19:04:30 -0700

    Attempt to make api call in PraseSearch component

--- src/components/PhraseSearch.js
+++ src/components/PhraseSearch.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function PhraseSearch() {
-  function handleSubmit(event) {
-    event.preventDefault();
-    alert("hello");
+export default function PhraseSearch(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [keyfrase, setKeyfrase] = useState(props.defaultKeyfrase);
+  const [answer, setAnswer] = useState(null);
+
+  function displayAnswer(response) {
+    setAnswer(response.data.answer);
   }
 
-  return (
-    <div className="PhraseSearch">
-      <form onSubmit={handleSubmit}>
-        <input type="search" />
-        <input type="submit" value="Phrase" />
-      </form>
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    handleFraseSubmit();
+  }
+
+  function explainPhrase() {
+    let apiKey = "182a2fb198a6etcbecec6a40a9o4bb3f";
+    let prompt = `Explain the meaning of the next phrase  ${keyfrase}`;
+    let context =
+      "Make sure to provide short answer. Do not repeat the phrase itself. Just say the meaning.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayAnswer);
+  }
+
+  function handleFraseSubmit(event) {
+    // event.preventDefault();
+    explainPhrase();
+  }
+
+  function handleKeyPhraseChange(event) {
+    setKeyfrase(event.target.value);
+  }
+
+  if (loaded) {
+    return (
+      <div className="PhraseSearch">
+        <section>
+          <form onSubmit={handleFraseSubmit}>
+            <input
+              type="search"
+              defaultValue={`raining cats and dogs`}
+              onChange={handleKeyPhraseChange}
+            />
+            <input type="submit" value="Phrase" />
+          </form>
+          <small className="hint">i.e. Hit the road</small>
+        </section>
+        <p>{answer}</p>
+      </div>
+    );
+  } else {
+    load();
+    return "Loading!";
+  }
 }
